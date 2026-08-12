6292e719122299b7578fdcabb589d07526787988
axax4037 <69842404+axax4037@users.noreply.github.com>
2026-08-12 23:12:09 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'shanshi-static-v20260812-d1-3';
+const CACHE_NAME = 'shanshi-static-v20260812-d1-5';
 const CORE_ASSETS = ['./', './index.html', './app.css'];
 
 self.addEventListener('install', event => {
