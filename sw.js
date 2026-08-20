aa7811865bf0173743b4760437990838e4c0a7e1
MHChen <MHChen@CCP.COM.TW>
2026-08-20 17:04:12 +0800

    Consolidate safety stock settings and improve QR labels

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'arenastone-static-v20260820-commercial-v4-14';
+const CACHE_NAME = 'arenastone-static-v20260820-commercial-v4-15';
 const CORE_ASSETS = [
   './',
   './index.html',
