88452b5a3c479c9f1966d62ee10f9b1e556aeb33
MHChen <MHChen@CCP.COM.TW>
2026-08-20 15:53:28 +0800

    Load dashboard alerts after initial sync

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'arenastone-static-v20260820-commercial-v4-5';
+const CACHE_NAME = 'arenastone-static-v20260820-commercial-v4-6';
 const CORE_ASSETS = [
   './',
   './index.html',
