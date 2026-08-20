c0d83484643c4daf69aa6a4ff7209697542ca72a
MHChen <MHChen@CCP.COM.TW>
2026-08-20 16:05:57 +0800

    Show inbound notes and clarify location management

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'arenastone-static-v20260820-commercial-v4-7';
+const CACHE_NAME = 'arenastone-static-v20260820-commercial-v4-8';
 const CORE_ASSETS = [
   './',
   './index.html',
